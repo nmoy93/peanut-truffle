cae080525eda8891c39731cf783bc546962a95c1
Nick Moy <nmoy@gmail.com>
2019-01-28 21:05:13 -0500

    Corrected the input() type for python 3

--- masterticket.py
+++ masterticket.py
@@ -3,15 +3,15 @@ tickets_remaining = 100
 
 print("Welcome! There are {} tickets to the concert remaining.".format(tickets_remaining))
 
-name = raw_input("Before we get started, what is your name?  ")
+name = input("Before we get started, what is your name?  ")
 
-num_tickets = raw_input("How many tickets will you be purchasing, {}?  ".format(name))
+num_tickets = input("How many tickets will you be purchasing, {}?  ".format(name))
 num_tickets = int(num_tickets)
 
 amount_due = ticket_price * num_tickets
 print("That will be ${}.".format(amount_due))
 
-proceed = raw_input("Do you want to proceed with your purchase? (Select Y/N)")
+proceed = input("Do you want to proceed with your purchase? (Select Y/N)")
 if proceed == "y" or "Y":
     print("CHA CHING - SOLD!")
 
